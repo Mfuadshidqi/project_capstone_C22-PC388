6a80fe3d70919a77657c35228a145da9a50d8b75
Milda Fauziah <fauziah.milda@gmail.com>
2022-05-28 16:05:30 +0700

    first commit

---
+++ DBConfig.php
@@ -0,0 +1,19 @@
+<?php 
+
+class DBConfig
+{
+    public $servername;
+    public $username;
+    public $password;
+    public $databasename;
+
+    public function __construct()
+    {
+        $this->servername = 'localhost';
+        $this->username = 'root';
+        $this->password = '';
+        $this->databasename = 'waste-app';
+    }
+}
+
+?>
