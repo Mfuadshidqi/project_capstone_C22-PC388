1613342a6a2d0015f2329f15b65870de266d8cb8
thariq.razan <thariq.razan@ui.ac.id>
2022-06-03 20:34:11 +0700

    add: ML model ver.2

--- app/src/main/java/com/fuad/mywasteappchanneling/ui/home/HomeViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.fuad.mywasteappchanneling.ui.home
-
-import android.app.Application
-import androidx.lifecycle.ViewModel
-
-class HomeViewModel(application: Application) : ViewModel() {
-
-
-}
